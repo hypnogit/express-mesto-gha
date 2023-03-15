5db703c9bc8a38899ad50bcbc81f5f5abc8aa7f9
Artur Shiabutdinov <hypnorus@yandex.ru>
2023-03-15 18:51:53 +0300

    project 13 iteration 4

--- controllers/users.js
+++ controllers/users.js
@@ -43,7 +43,7 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateProfile = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about, avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about, avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         res.status(notFoundError).send({
@@ -64,7 +64,7 @@ module.exports.updateProfile = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         res.status(notFoundError).send({
